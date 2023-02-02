f8b84b81e0aaa097d139916f3fd5b7b0d7642b46
dev <2006@gmail.com>
2023-02-02 21:42:53 +0500

    yangi

--- wheather.js
+++ wheather.js
@@ -3,18 +3,19 @@ btn = document.querySelector('#btn'),
 temp = document.querySelector('.temp'),
 input = document.querySelector('#input'),
 loader = document.querySelector('#loader')
-API_key = '9c53ebe02251296d3e778beec1e3dab0'
+API_key = 'e7b0d101c09859e4dca2fc973500c19d'
 
 
 btn.addEventListener('click', (e)=>{
   e.preventDefault()
   city.innerHTML = ''
+  temp.innerHTML = ''
   country = input.value
   loader.style.display = 'inline-block'
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${API_key}`)
   .then(res=> res.json())
   .then(data=> {
-    loader.style.display = 'none'
+      loader.style.display = 'none'
       city.innerHTML = data.name
       temp.innerHTML = (data.main.temp_min/10).toFixed(0)+'°C/'+(data.main.temp_max/10).toFixed(0)+'°C'
   })
